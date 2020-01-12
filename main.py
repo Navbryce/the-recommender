945b25f1e8bc89ec869bded8ae83784caab7136e
Navbryce <plunkett.bryce.m@gmail.com>
2020-01-12 13:53:16 -0500

    Skeleton of project

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+The Recommender
+Python >= 3.6
+"""
+
+__author__ = "Bryce"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Entry point of app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
