8012a71df9428c9656d18a683aef83c677e21caa
Bryce Plunkett <plunkett.bryce.m@gmail.com>
2020-05-15 00:07:26 -0400

    started api

--- recommender/data/business_search_request.py
+++ recommender/data/business_search_request.py
@@ -1,6 +1,14 @@
-from recommender.data import data_object
+from typing import Dict
+
+from recommender.data.data_object import DataObject
+from recommender.data.location import Location
 
 
-@data_object("location", "price_categories", "categories", "attributes")
+@DataObject(["search_term", "location", "price_categories", "categories", "attributes"])
 class BusinessSearchRequest:
-    pass
+    @staticmethod
+    def from_json(json_dict: Dict):
+        request = BusinessSearchRequest()
+        request._search_term = json_dict["search_term"]
+        request._location = Location.from_json(json_dict["location"])
+        return request.build()
--- recommender/data/location.py
+++ recommender/data/location.py
@@ -1,3 +1,8 @@
 from collections import namedtuple
+from typing import Dict
 
 Location = namedtuple("Location", ["lat", "long"])
+
+
+def from_json(json_dict: Dict) -> Location:
+    return Location(**json_dict)
